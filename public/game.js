ea703ecd309bfe5ea08f1fb174b5072ef67087c1
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-12-08 17:16:56 +0100

    Add kaboom package, write start game function speed indentifiers, game logic and load sprites

--- public/game.js
+++ public/game.js
@@ -33,7 +33,6 @@ function register (event) {
         event.target.elements['register-password'].value,
         event.target.elements['register-username'].value,
     ).then(response => {
-        console.log({ response })
         databases.createDocument(databaseId, collectionId, generateUUID(), {
             'userId': response.$id,
             'highScore': 0
@@ -42,10 +41,61 @@ function register (event) {
         account.createEmailSession(
             event.target.elements['register-email'].value,
             event.target.elements['register-password'].value,
-        )
+        ).then(() => {
+            showDisplay()
+        })
     }).catch(error => {
         console.log(error)
     })
     event.preventDefault()
 }
 
+function showDisplay () {
+    const modalElement = document.getElementById('modal')
+    modalElement.classList.add('hidden')
+}
+
+showDisplay()
+
+//Kaboom game
+function startGame () {
+    kaboom({
+        global: true,
+        fullscreen: true,
+        scale: 2,
+    })
+
+    //Speed indentifiers
+    const moveSpeed = 120
+    const jumpForce = 360
+    const bigJumpForce = 550
+    let currentJumpForce = jumpForce
+    const fallDeath = 400
+    const enemyDeath = 20
+
+    //Game logic
+    let isJumping = true
+
+    //Load sprites
+    loadRoot('https://i.imgur.com/')
+    loadSprite('coin', 'wbKxhcd.png')
+    loadSprite('evil-shroom', 'KPO3fR9.png')
+    loadSprite('brick', 'pogC9x5.png')
+    loadSprite('block', 'M6rwarW.png')
+    loadSprite('mario', 'Wb1qfhK.png')
+    loadSprite('mushroom', '0wMd92p.png')
+    loadSprite('surprise', 'gesQ1KP.png')
+    loadSprite('unboxed', 'bdrLpi6.png')
+    loadSprite('pipe-top-left', 'ReTPiWY.png')
+    loadSprite('pipe-top-right', 'hj2GK4n.png')
+    loadSprite('pipe-bottom-left', 'c1cYSbt.png')
+    loadSprite('pipe-bottom-right', 'nqQ79eI.png')
+    loadSpirte('blue-block', 'fVscIbn.png')
+    loadSprite('blue-brick', '3e5YRQd.png')
+    loadSprite('blue-steel', 'gqVoI2b.png')
+    loadSprite('blue-evil-shroom', 'SvV4ueD.png')
+    loadSprite('blue-surprise', 'RMqCc1G.png')
+
+}
+
+startGame()
